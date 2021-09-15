4a14e3d9c721f9a46c555b4fac20b5725bb82746
13129921509 <38420506+13129921509@users.noreply.github.com>
2021-09-15 17:30:35 +0800

    修改消息转实体类的bean

--- src/main/java/com/cai/ais/core/AisConfiguration.java
+++ src/main/java/com/cai/ais/core/AisConfiguration.java
@@ -1,10 +1,16 @@
 package com.cai.ais.core;
 
+import com.cai.ais.config.AisMessage;
 import com.cai.ais.config.AisProperties;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.ClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.amqp.support.converter.SimpleMessageConverter;
@@ -53,7 +59,20 @@ public class AisConfiguration {
 
     @Bean
     MessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        Jackson2JsonMessageConverter messageConverter = new Jackson2JsonMessageConverter(objectMapper);
+        messageConverter.setClassMapper(new ClassMapper() {
+            @Override
+            public void fromClass(Class<?> clazz, MessageProperties properties) {
+            }
+
+            @Override
+            public Class<?> toClass(MessageProperties properties) {
+                return AisMessage.class;
+            }
+        });
+        return messageConverter;
     }
 
 
